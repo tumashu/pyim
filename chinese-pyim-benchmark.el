7c10b3813ca686cfc7df1996a4c516b18faa7f90
Feng Shu <tumashu@163.com>
2016-06-21 08:29:12 +0800

    update pyim-benchmark.el

--- chinese-pyim-benchmark.el
+++ chinese-pyim-benchmark.el
@@ -65,13 +65,42 @@
     (message "## Benchmark `pyim-get-choices:dabbrev' ...")
     (benchmark time '(pyim-get-choices:dabbrev pylist))
 
-    (message "## Benchmark `pyim-get-dabbrev' ...")
-    (benchmark time '(pyim-get-dabbrev
-                      (pyim-build-chinese-regexp-for-pylist pylist nil nil t)
-                      pyim-dabbrev-time-limit
-                      (pcase pyim-dabbrev-other-buffers
-                        (`t (list major-mode))
-                        (`all `all))))
+    (message "## Benchmark `pyim-get-dabbrev' 1 ...")
+    (benchmark time '(save-excursion
+                       (pyim-get-dabbrev
+                        (pyim-build-chinese-regexp-for-pylist pylist nil nil t)
+                        pyim-dabbrev-time-limit)))
+
+    (message "## Benchmark `pyim-get-dabbrev' 2  ...")
+    (benchmark time '(save-excursion
+                       (pyim-get-dabbrev
+                        "[你呢尼泥逆倪匿拟腻妮霓昵溺旎睨鲵坭猊怩伲祢慝铌年念廿粘碾捻蔫撵拈黏鲶鲇辇埝娘酿鸟尿袅嬲茑脲捏涅聂孽蹑嗫啮镊镍乜陧颞臬蘖您恁宁凝拧泞咛狞柠佞聍苎甯牛纽扭妞钮拗忸狃][好号毫豪浩耗皓嚎昊郝壕蒿貉灏镐嗥嚆薅濠蚝颢]"
+                        pyim-dabbrev-time-limit)))
+
+    (message "## Benchmark `pyim-get-dabbrev-time-limit-while' ...")
+    (benchmark time '(let ((tmp-end (point))
+                           (start (current-time))
+                           symbols)
+                       (pyim-get-dabbrev-time-limit-while (> tmp-end (point-min))
+                           start pyim-dabbrev-time-limit 1
+                           (ignore-errors
+                             (forward-char -10000))
+                           (forward-line 0)
+                           (save-excursion
+                             ;; Before, we used backward search, but it matches non-greedily, and
+                             ;; that forced us to use the "beginning/end of word" anchors in
+                             ;; search regexp.
+                             (while (re-search-forward " " tmp-end t)
+                               (let ((match (match-string-no-properties 0)))
+                                 (when (>= (length match) 2)
+                                   (push match symbols)))))
+                           (setq tmp-end (point)))))
+
+    (message "## Benchmark `re-search-forward' chinese chars ...")
+    (benchmark time '(save-excursion
+                       (re-search-forward
+                        (pyim-build-chinese-regexp-for-pylist pylist nil nil t)
+                        nil t)))
 
     (message "## Benchmark `pyim-get-choices:pinyin-dict' ...")
     (benchmark time '(pyim-get-choices:pinyin-dict pylist))
@@ -85,12 +114,16 @@
     (message "## Benchmark `pyim-cchar2pinyin-get' ...")
     (benchmark time '(pyim-cchar2pinyin-get ?你))
 
+    (message "## Benchmark `pyim-pinyin2cchar-get' ...")
+    (benchmark time '(pyim-pinyin2cchar-get "ni"))
+
     (message "## Benchmark `pyim-build-chinese-regexp-for-pylist' ...")
     (benchmark time '(pyim-build-chinese-regexp-for-pylist pylist nil nil t))
 
     (message "## Benchmark `pyim-get' ...")
     (benchmark time '(pyim-get "ni-hao" '(pinyin-dict)))
-    (message "=================================================")))
+    (message "=================================================")
+    (message "")))
 
 ;; #+END_SRC
 
