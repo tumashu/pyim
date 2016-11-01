50a66b92e08b88954829b12e43fddfccc758bf58
Feng Shu <tumashu@163.com>
2016-11-01 09:55:01 +0800

    对我自己的 pyim 探针设置作一些说明

--- chinese-pyim.el
+++ chinese-pyim.el
@@ -99,7 +99,11 @@
 ;;   ;; 我使用全拼
 ;;   (setq pyim-default-scheme 'quanpin)
 
-;;   ;; 设置 pyim 探针设置，可以实现 *无痛* 中英文切换 :-)
+;;   ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+;;   ;; 我自己使用的中英文动态切换规则是：
+;;   ;; 1. 光标只有在注释里面时，才可以输入中文。
+;;   ;; 2. 光标前是汉字字符时，才能输入中文。
+;;   ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
 ;;   (setq-default pyim-english-input-switch-functions
 ;;                 '(pyim-probe-dynamic-english
 ;;                   pyim-probe-isearch-mode
