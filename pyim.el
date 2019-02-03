c0d4d729b1d9b769aa717e9a9aa2ce6f07992329
Feng Shu <tumashu@163.com>
2019-02-03 12:19:39 +0800

    实现 yefeiyu 同学描述的“四码上屏”功能

--- pyim.el
+++ pyim.el
@@ -2233,9 +2233,10 @@ Return the input string.
            (= (length pyim-entered) n))
       ;; 五笔等型码输入法，重码率很低，90%以上的情况都是选择第一个词条，
       ;; 这里添加自动选择功能，减少按空格强制选词的机会。
+      (push last-command-event unread-command-events)
       (unless (equal pyim-candidates (list pyim-entered))
         (pyim-outcome-handle 'candidate))
-      (pyim-entered-handle (char-to-string last-command-event)))
+      (pyim-terminate-translation))
      ((pyim-input-chinese-p)
       (pyim-entered-handle
        (concat pyim-entered
