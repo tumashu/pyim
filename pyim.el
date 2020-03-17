1bd2b354d14860dceda54b02c080f42bf1db8c1c
Feng Shu <tumashu@163.com>
2020-03-17 21:21:21 +0800

    Simplify pyim-liberime-get-code

--- pyim.el
+++ pyim.el
@@ -3488,8 +3488,7 @@ minibuffer 原来显示的信息和 pyim 选词框整合在一起显示
 (defun pyim-liberime-get-code (word input &optional limit)
   "Get the code of WORD from the beginning of INPUT.
 `liberime-search' with LIMIT argument is used internal."
-  (let* ((test-words (liberime-search "nih" limit))
-         (n (length word))
+  (let* ((n (length word))
          (i (min (length input) (* n 5)))
          words str result1 result2)
     (while (> i 0)
@@ -3507,11 +3506,12 @@ minibuffer 原来显示的信息和 pyim 选词框整合在一起显示
      ;; 不同的输入法处理方式也不一样，这里使用了一个笨办法做探测当前输
      ;; 入法：获取 "nih" 对应的词条列表，如果词条列表中包含 “你好”，那
      ;; 么当前输入法可能是全屏或者双拼。
-     ((member "你好" test-words)
+     ((member "你好" (liberime-search "nih" limit))
       (or (car (reverse result1))
           (car (reverse result2))))
-     (t (or (car (reverse result2))
-            (car (reverse result1)))))))
+     (t
+      (or (car (reverse result2))
+          (car (reverse result1)))))))
 
 ;; (pyim-liberime-get-code "你好" "nihaoma")
 
