ee9bb963c82b90adcf90f18cf21f4855d5249a6d
Feng Shu <tumashu@163.com>
2021-02-22 11:01:48 +0800

    * pyim-pkg.el: Do not depend no GNU elpa package.

--- pyim-pkg.el
+++ pyim-pkg.el
@@ -1,7 +1,5 @@
 (define-package "pyim" "2.0"
   "A Chinese input method support quanpin, shuangpin, wubi and cangjie."
   '((emacs         "24.4")
-    (popup         "0.1")
     (async         "1.6")
-    (xr            "1.13")
-    (pyim-basedict "0.1")))
+    (xr            "1.13")))
