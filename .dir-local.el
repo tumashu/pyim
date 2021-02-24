5c2bc3b9fea10531abcba4d21aeb1fbe8f9686bd
Feng Shu <tumashu@163.com>
2021-02-24 13:33:06 +0800

    .dir-local.el -> .dir-locals.el

--- .dir-local.el
+++
@@ -1,8 +0,0 @@
-((nil . ((fill-column . 80)
-         (indent-tabs-mode . nil)
-         (tab-width . 4)
-         (buffer-file-coding-system . utf-8-unix))))
-
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
