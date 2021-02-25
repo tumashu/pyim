fe98dd3f7cc2cc09e8a3f2cd71d420c757b2190f
Feng Shu <tumashu@163.com>
2021-02-25 10:23:35 +0800

    .dir-locals.el remove copyright info.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,26 +1,3 @@
-;; Copyright (C) 2019-2020 Free Software Foundation, Inc.
-
-;; Author: Chen Bin <chenbin.sh@gmail.com>
-;; Maintainer: Chen Bin <chenbin.sh@gmail.com>
-;;             Feng Shu <tumashu@163.com>
-;; URL: https://github.com/tumashu/pyim
-;; Keywords: convenience, Chinese, pinyin, input-method
-
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; GNU Emacs is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-
 ((nil . ((fill-column . 80)
          (indent-tabs-mode . nil)
          (tab-width . 4)
