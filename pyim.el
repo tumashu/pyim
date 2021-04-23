689980a517cdd270b4391620a1c9d9d2c9c340cd
Feng Shu <tumashu@163.com>
2021-04-24 02:42:58 +0800

    Fix pyim-posframe-valid-p error.
    	* pyim.el (pyim-start, pyim-terminate-translation): Use pyim-posframe-workable-p.
    
    	* pyim-page.el (pyim-page-posframe-workable-p): removed.
    	(pyim-page-tooltip-show):
    
    	* pyim-common.el (pyim-posframe-workable-p): New function.

--- pyim.el
+++ pyim.el
@@ -243,7 +243,7 @@ pyim 使用函数 `pyim-start' 启动输入法的时候，会将变量
     (add-hook 'minibuffer-exit-hook #'pyim-exit-from-minibuffer))
   (run-hooks 'pyim-active-hook)
   (when (and (eq pyim-page-tooltip 'posframe)
-             (not (pyim-page-posframe-workable-p)))
+             (not (pyim-posframe-workable-p)))
     (message "PYIM: posframe 没有正确安装或者当前 Emacs 版本不支持 posframe。"))
   (when restart
     (message "pyim 重启完成。"))
@@ -630,7 +630,7 @@ Return the input string.
   (setq pyim-assistant-scheme-enable nil)
   (setq pyim-force-input-chinese nil)
   (when (and (eq pyim-page-tooltip 'posframe)
-             (pyim-posframe-valid-p))
+             (pyim-posframe-workable-p))
     (posframe-hide pyim-page-tooltip-posframe-buffer))
   (pyim-entered-erase-buffer)
   (let* ((class (pyim-scheme-get-option (pyim-scheme-name) :class))
