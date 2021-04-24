675554f54abb35bf0372e9c361abab06744287de
Feng Shu <tumashu@163.com>
2021-04-24 20:32:28 +0800

    Revert "Add requires to pyim-imobjs.el"
    This reverts commit 8b0a7929daadc5a437bf6f31f00860c8b698d860.

--- pyim-imobjs.el
+++ pyim-imobjs.el
@@ -29,7 +29,6 @@
 ;; * 代码                                                           :code:
 (require 'cl-lib)
 (require 'pyim-common)
-(require 'pyim-scheme)
 
 (defgroup pyim-imobjs nil
   "Imobjs lib for pyim."
