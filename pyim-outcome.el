d422239e883f06915735f266460f9e1fbd0d49a3
Feng Shu <tumashu@163.com>
2021-04-24 20:33:39 +0800

    Revert "Add requires to pyim-outcome.el"
    This reverts commit b042bfe8f0890a735e4ac267ee6caba3ec654fc3.

--- pyim-outcome.el
+++ pyim-outcome.el
@@ -29,10 +29,6 @@
 ;; * 代码                                                           :code:
 (require 'cl-lib)
 (require 'pyim-common)
-(require 'pyim-scheme)
-(require 'pyim-entered)
-(require 'pyim-candidates)
-(require 'pyim-punctuation)
 
 (defgroup pyim-outcome nil
   "Outcome tools for pyim."
