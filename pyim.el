d0eabdb9aa6bbf11a72b2ae179c5bd21e10d6d7c
Feng Shu <tumashu@163.com>
2021-04-28 16:39:43 +0800

    v3.7

--- pyim.el
+++ pyim.el
@@ -7,7 +7,7 @@
 ;;         Feng Shu <tumashu@163.com>
 ;; Maintainer: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/pyim
-;; Version: 3.6
+;; Version: 3.7
 ;; Keywords: convenience, Chinese, pinyin, input-method
 ;; Package-Requires: ((emacs "24.4") (async "1.6") (xr "1.13"))
 
