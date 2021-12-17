0c5df7a1da98fbbb695b5d5ee20d8581c26a0c38
Feng Shu <tumashu@163.com>
2021-12-17 10:03:42 +0800

    * pyim-dhashcache.el (pyim-dhashcache-init-variables): 发现备份文件时通知用户。

--- pyim-dhashcache.el
+++ pyim-dhashcache.el
@@ -388,6 +388,10 @@ code 对应的中文词条了。
 
 (defun pyim-dhashcache-init-variables ()
   "初始化 dcache 缓存相关变量."
+  (when (and pyim-dcache-directory
+             (directory-files pyim-dcache-directory nil "-backup-"))
+    (message "PYIM: 在 %S 目录中发现备份文件的存在，可能是词库缓存文件损坏导致，请抓紧检查处理！！！"
+             pyim-dcache-directory))
   (pyim-dcache-init-variable pyim-dhashcache-iword2count)
   (pyim-dcache-init-variable pyim-dhashcache-code2word)
   (pyim-dcache-init-variable pyim-dhashcache-word2code)
