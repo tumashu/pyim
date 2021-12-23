c62801bc3e1e5bcb2f1b9f5f33b2cf28389b15d2
Feng Shu <tumashu@163.com>
2021-12-23 15:04:43 +0800

    删除 hack for xshell or MobaXTerm.
    	* pyim-hacks.el (pyim-hacks-for-ssh)
    	(pyim-hacks-add-unread-command-events)
    	(pyim-add-unread-command-events): Removed.
    
    今天我下载两个软件测试了一下，可以正常使用，不需要这个 hack 了。

--- pyim-hacks.el
+++ pyim-hacks.el
@@ -30,32 +30,6 @@
 
 ;;; Code:
 ;; * 代码                                                                 :code:
-
-;; ** Hack for xshell or MobaXTerm.
-(defvar pyim-hacks-for-ssh nil
-  "Hack of [<no-record> is undefined #402](https://github.com/tumashu/pyim/issues/402)
-
-When user use Xshell or MobaXTerm, error '<no-record> is undefined' will be
-exist, this may be not a pyim's bug.  but I do not know how to solve this
-problem, so I do this ugly hack, and wait others help ...
-1. https://github.com/tumashu/pyim/issues/402
-2. https://git.savannah.gnu.org/cgit/emacs.git/commit/?id=bd5c7404195e45f11946b4e0933a1f8b697d8b87x")
-
-(defun pyim-hacks-add-unread-command-events (orig_func key &optional reset)
-  "Advice function of `pyim-add-unread-command-events'."
-  (if (not pyim-hacks-for-ssh)
-      (funcall orig_func key reset)
-    (when reset
-      (setq unread-command-events nil))
-    (setq unread-command-events
-          (if (characterp key)
-              (cons key unread-command-events)
-            (append (mapcan (lambda (e) (list e))
-                            (append key nil))
-                    unread-command-events)))))
-
-(advice-add 'pyim-add-unread-command-events :around #'pyim-hacks-add-unread-command-events)
-
 ;; * Footer
 (provide 'pyim-hacks)
 
