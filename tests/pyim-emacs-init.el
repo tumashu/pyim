8d76a25bb835ec8dd1b6c2f5cd570e0c797381af
Chen Bin <chenbin.sh@gmail.com>
2022-06-23 16:45:36 +1000

    run specific version of emacs to test latest pyim code

---
+++ tests/pyim-emacs-init.el
@@ -0,0 +1,10 @@
+(require 'pyim)
+(setq default-input-method "pyim")
+
+(defun pyim-test-find-file (file)
+  "Read FILE's content into current buffer."
+  (let* ((files (directory-files-recursively default-directory file)))
+    (file-truename (car files))))
+
+(setq pyim-dicts (list (list :name "basedict" :file (pyim-test-find-file "pyim-basedict.pyim"))))
+(message "pyim-dicts=%s" pyim-dicts)
