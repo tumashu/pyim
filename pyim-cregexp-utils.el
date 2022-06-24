b243e7e8e7e27ea5c50bf67ec1bee81eb069fabc
Feng Shu <tumashu@163.com>
2022-06-24 17:07:50 +0800

    pyim-isearch-search-fun -> pyim-isearch--search-fun

--- pyim-cregexp-utils.el
+++ pyim-cregexp-utils.el
@@ -90,11 +90,11 @@ buffer."
   :lighter " pyim-isearch"
   (if pyim-isearch-mode
       (progn
-        (advice-add 'isearch-search-fun :override #'pyim-isearch-search-fun)
+        (advice-add 'isearch-search-fun :override #'pyim-isearch--search-fun)
         (message "PYIM: `pyim-isearch-mode' 已经激活，激活后，一些 isearch 扩展包有可能失效。"))
-    (advice-remove 'isearch-search-fun #'pyim-isearch-search-fun)))
+    (advice-remove 'isearch-search-fun #'pyim-isearch--search-fun)))
 
-(defun pyim-isearch-search-fun ()
+(defun pyim-isearch--search-fun ()
   "这个函数为 isearch 相关命令添加中文拼音搜索功能，
 做为 `isearch-search-fun' 函数的 advice 使用。"
   (funcall
